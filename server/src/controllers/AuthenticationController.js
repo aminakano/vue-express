5f544eb45834abce1b6973203e180cf2616a7ef6
Ami <ami.nakano@outlook.com>
2019-11-28 16:11:31 +0100

    backend login register worked

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,11 @@ module.exports = {
     async register(req, res){
         try {
             const user = await User.create(req.body);
-            res.send(user.toJSON());
+            const userJson = user.toJSON();
+            res.send({
+            user: userJson,
+            token: jwtSignUser(userJson)
+            });
         } catch (err){
             res.status(400).send({
                 error: "This email account is already in use."
@@ -22,14 +26,12 @@ module.exports = {
     },
     async login(req, res) {
         try {
-            console.log('req',req.body)
           const {email, password} = req.body;
           const user = await User.findOne({
               where: {
                   email: email
               }
           });
-          console.log('user:',user, 'email:',req.body.email)
           if(!user) {
               return res.status(403).send({
                   error: "The user information is incorrect."
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -23,8 +23,6 @@ module.exports = (sequelize, DataTypes) =>{
         password: DataTypes.STRING
     }, {
         hooks: {
-            beforeCreate: hashPassword,
-            beforeUpdate: hashPassword,
             beforeSave: hashPassword
         }
     })
